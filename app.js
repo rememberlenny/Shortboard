3be1d759b6958f1ff12e26e7167f45aaf6697df5
RememberLenny <leonard_bogdonoff@condenast.com>
2014-03-28 09:38:45 -0400

    Comment the functions with descriptions

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-// Define the target for shortcut
-
-// Choose the keyboard to press
-
-// Choose whether last
+// a. Define the target for shortcut
+// b. Choose the keyboard to press
+// c. Choose whether last
 
 
 ;(function($){
@@ -15,8 +13,28 @@
       // key: action
     },
 
-    /* On keypress, check keyActions
-     * @keyPressed: Button pressed
+    shortboardWrap: '<div id="shortboard-wrap"></div>',
+    shortboardStatus: '<span id="shortboard-status"></span>',
+    shortboardList: '<ul id="shortboard-list"></ul>',
+    shortboardItem: '<li class="shortboard-item"></li>',
+    /**
+     * Template for shortboard controls
+     * Field for css selector and keyboard shortcut
+     */
+    shortboardCreatePanel:
+      '<div id="shortboard-controls">'+
+      ' <div class="col-50">'+
+      '   <input id="shortboard-shortcut" type="text"></input>'+
+      ' </div>'+
+      ' <div class="col-50">'+
+      '   <input id="shortboard-selector" type="text"></input>'+
+      ' </div>'+
+      '</div>',
+
+
+    /**
+     *  On keypress, check keyActions
+     *  @keyPressed: Button pressed
      */
     keyboardEvents: function(keyPressed){
       $.on('keypress', function(e){
@@ -26,49 +44,57 @@
       });
     }
 
-    /* Run Initialize on load
+    /**
+     * Run Initialize on load
      * @clickTarget: CSS selector for click target
      */
     initialize: function(clickTarget){
       this.setClickTarget(clickTarget);
     },
 
-    /* Load UI to determine shortcuts
+    /**
+     * Load UI to determine shortcuts
      * @clickTarget: CSS selector for click target
      */
     loadShortcutDefiner: function(){
 
+      $('body').append('')
     },
 
-    /* Associate keyboard key to css element on page
+    /**
+     * Associate keyboard key to css element on page
      * @clickTarget: CSS selector for click target
      */
     saveKeyClickPair: function(clickTarget, keyboardTarget){
       this.keyActions[keyboardTarget] = clickTarget;
     },
 
-    /* Define the click target
+    /**
+     * Define the click target
      * @clickTarget: CSS selector for click target
      */
     setClickTarget: function(clickTarget){
       return clickTarget;
     },
 
-    /* Click the defined target
+    /**
+     * Click the defined target
      * @keyboardTarget: Keyboard button action
      */
     doClickTarget: function(clickTarget){
       $(clickTarget).click();
     }
 
-    /* Set the keyboard button to trigger
+    /**
+     * Set the keyboard button to trigger
      * @keyboardTarget: Keyboard button action
      */
     setKeyboardTarget: function(pressedKey){
       return pressedKey;
     },
 
-    /* Set length of timeout between click
+    /**
+     * Set length of timeout between click
      * @timeBetween: Time between clicks in seconds
      */
     setBetweenClickCondition: function(timeBetween){
@@ -78,7 +104,8 @@
       }, timeBetween);
     },
 
-    /* Select if last key
+    /**
+     * Select if last key
      * @isLast: Select if last
      */
     chooseIfLast: function(isLast){
