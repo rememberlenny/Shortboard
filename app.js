637b12afcd3adabe0e5051577a5fbb8699987c86
RememberLenny <leonard_bogdonoff@condenast.com>
2014-03-28 09:38:18 -0400

    First commit: High level functions mapped

---
+++ app.js
@@ -0,0 +1,90 @@
+// Define the target for shortcut
+
+// Choose the keyboard to press
+
+// Choose whether last
+
+
+;(function($){
+
+  var shortBoard = {} || shortBoard;
+
+  shortBoard = {
+
+    keyActions: {
+      // key: action
+    },
+
+    /* On keypress, check keyActions
+     * @keyPressed: Button pressed
+     */
+    keyboardEvents: function(keyPressed){
+      $.on('keypress', function(e){
+        // e.target;
+        // if( pressedKey === up ){
+        // }
+      });
+    }
+
+    /* Run Initialize on load
+     * @clickTarget: CSS selector for click target
+     */
+    initialize: function(clickTarget){
+      this.setClickTarget(clickTarget);
+    },
+
+    /* Load UI to determine shortcuts
+     * @clickTarget: CSS selector for click target
+     */
+    loadShortcutDefiner: function(){
+
+    },
+
+    /* Associate keyboard key to css element on page
+     * @clickTarget: CSS selector for click target
+     */
+    saveKeyClickPair: function(clickTarget, keyboardTarget){
+      this.keyActions[keyboardTarget] = clickTarget;
+    },
+
+    /* Define the click target
+     * @clickTarget: CSS selector for click target
+     */
+    setClickTarget: function(clickTarget){
+      return clickTarget;
+    },
+
+    /* Click the defined target
+     * @keyboardTarget: Keyboard button action
+     */
+    doClickTarget: function(clickTarget){
+      $(clickTarget).click();
+    }
+
+    /* Set the keyboard button to trigger
+     * @keyboardTarget: Keyboard button action
+     */
+    setKeyboardTarget: function(pressedKey){
+      return pressedKey;
+    },
+
+    /* Set length of timeout between click
+     * @timeBetween: Time between clicks in seconds
+     */
+    setBetweenClickCondition: function(timeBetween){
+      var timeBetween = 1000 * timeBetween;
+      setTimeout(function(){
+
+      }, timeBetween);
+    },
+
+    /* Select if last key
+     * @isLast: Select if last
+     */
+    chooseIfLast: function(isLast){
+
+    }
+
+  }
+
+})(jQuery);
