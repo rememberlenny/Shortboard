f422736c2ecb78cf3a1f582cf9c03694f1e2f689
RememberLenny <leonard_bogdonoff@condenast.com>
2014-04-11 23:54:05 -0400

    Refactor index

--- app.js
+++ app.js
@@ -1,158 +1,180 @@
-// a. Define the target for shortcut
-// b. Choose the keyboard to press
-// c. Choose whether last
 
+    var keyCombos = [];
+    var savedKeys = [];
+    var keys      = {};
+    var pastKey   = [];
+    var tempKey = [];
+    var keyComboDisplay = [];
+    var isWatch   = false;
+    var isSelectable = false;
+    var $displayKey = $('#display-current-key-LKBG');
 
-;(function($){
+    // **
+    // * Keybaord related events
+    // *
 
-  var shortBoard = {} || shortBoard;
+    $(document).keydown(function (e) {
+      keys[e.which] = true;
+      printKeys();
+    });
 
-  shortBoard = {
+    $(document).keyup(function (e) {
+      delete keys[e.which];
+    });
 
-    keyActions: [
-      {
-        // key: 'enter',
-        // action: '#home',
-      }
-    ],
-
-    /**
-     *   # Shortcut Definer
-     *
-     *   ________________
-     *   |    *status*    \
-     *   __________________
-     *   |  + list-item   |
-     *   |  + list-item   |
-     *   |  + list-item   |
-     *   __________________
-     *   | [ input: css ] |
-     *   | [ input: key ] |
-     *   |     [ submit ] |
-     *   __________________
-     */
-
-    shortboardWrap: '<div id="shortboard-wrap"></div>',
-    shortboardStatus: '<span id="shortboard-status"></span>',
-    shortboardList: '<ul id="shortboard-list"></ul>',
-    shortboardItem: '<li class="shortboard-item"></li>',
-    /**
-     * Template for shortboard controls
-     * Field for css selector and keyboard shortcut
-     */
-    shortboardCreatePanel:
-      '<div id="shortboard-controls">'+
-      ' <div class="col-50">'+
-      '   <input id="shortboard-shortcut" type="text"></input>'+
-      ' </div>'+
-      ' <div class="col-50">'+
-      '   <input id="shortboard-selector" type="text"></input>'+
-      ' </div>'+
-      ' <div class="col-100">'+
-      '   <button id="shortboard-submit"></button>'+
-      ' </div>'+
-      '</div>',
-
-
-    /**
-     *  On keypress, check keyActions
-     *  @keyPressed: Button pressed
-     */
-    keyboardEvents: function(keyPressed){
-      $.on('keypress', function(e){
-        // e.target;
-        // if( pressedKey === up ){
-        // }
-      });
-    },
-
-    /**
-     * Run Initialize on load
-     * @clickTarget: CSS selector for click target
-     */
-    initialize: function(clickTarget){
-      this.setClickTarget(clickTarget);
-    },
-
-    /**
-     * Load UI to determine shortcuts
-     * @clickTarget: CSS selector for click target
-     */
-    loadShortcutDefiner: function(){
+    $('#watch-button-LKBG').on('click', function(event){
       var self = this;
-      $('body').append(self.shortboardWrap);
-      var $container = $('#shortboard-wrap');
-      $container.append(self.shortboardStatus);
-      $container.append(self.shortboardList);
-      var $list = $('#shortboard-list');
+      beginWatchKeys();
+    });
 
-      if(self.keyActions.length > 0){
+    $('#stop-button-LKBG').on('click', function(event){
+      var self = this;
+      stopWatchKeys();
+    });
 
-        $.each(self.keyActions, function(i, item){
-          $list.append(self.shortboardItem);
-          var $lastItem = $('shortboard-item').last();
-          $lastItem.append(
-            self.keyActions[i]['key']
-            );
-          $lastItem.append(
-            self.keyActions[i]['action']
-            );
-        });
+    $('#clear-button-LKBG').on('click', function(event){
+      var self = this;
+      clearKeyCombos(event, self);
+    });
+
+    $('#save-button-LKBG').on('click', function(event){
+      var self = this;
+      saveKeyCombo(event, self);
+    });
+
+    // **
+    // * Mouse related events
+    // *
+
+    $('#select-target-button-LKBG').on('click', function(event){
+      selectTarget();
+    });
+
+    $('*').hover(
+      function(){
+        if(isSelectable==true){
+          $(this).css('border', '1px solid red');
+        }
+      },
+      function(){
+        if(isSelectable==true){
+          $(this).css('border', 'none');
+        }
       }
+    );
 
-    },
+    $('*').on('click', function(e){
+      if(isSelectable==true){
+        console.log(e.target);
+        return false;
+      }
+    });
 
-    /**
-     * Associate keyboard key to css element on page
-     * @clickTarget: CSS selector for click target
-     */
-    saveKeyClickPair: function(clickTarget, keyboardTarget){
-      this.keyActions[keyboardTarget] = clickTarget;
-    },
+    // **
+    // * Mouse related actionss
+    // *
 
-    /**
-     * Define the click target
-     * @clickTarget: CSS selector for click target
-     */
-    setClickTarget: function(clickTarget){
-      return clickTarget;
-    },
-
-    /**
-     * Click the defined target
-     * @keyboardTarget: Keyboard button action
-     */
-    doClickTarget: function(clickTarget){
-      $(clickTarget).click();
-    },
-
-    /**
-     * Set the keyboard button to trigger
-     * @keyboardTarget: Keyboard button action
-     */
-    setKeyboardTarget: function(pressedKey){
-      return pressedKey;
-    },
-
-    /**
-     * Set length of timeout between click
-     * @timeBetween: Time between clicks in seconds
-     */
-    setBetweenClickCondition: function(timeBetween){
-      var timeBetween = 1000 * timeBetween;
-      setTimeout(function(){
-
-      }, timeBetween);
-    },
-
-    /**
-     * Select if last key
-     * @isLast: Select if last
-     */
-    chooseIfLast: function(isLast){
-
+    function createBorder(e, self){
+      console.log(self);
+      if(e.type == 'mouseenter'){
+        $(self).css('border', '1px solid red');
+      } else {
+        $(self).css('border', 'none');
+      }
     }
 
-  }
+    function scanForClick(){
+      if(isSelectable == true){
+      }
+    }
 
-})(jQuery);
+    function selectTarget(){
+      isSelectable = true;
+      scanForClick();
+    }
+
+    // **
+    // * Keyboard related Actions
+    // *
+
+    function printKeys() {
+      if(isWatch == true){
+        tempKey = [];
+        for (var i in keys) {
+          if (!keys.hasOwnProperty(i)) continue;
+          tempKey.push(i);
+        }
+        displayTempKey(tempKey);
+      }
+    }
+
+    function displayTempKey(tempKey){
+      $displayKey.html(tempKey);
+      pastKey = tempKey;
+      console.log(tempKey);
+    }
+
+    function beginWatchKeys(){
+      isWatch = true;
+      hideWatch();
+    }
+
+    function stopWatchKeys(){
+      isWatch = false;
+      showWatch();
+    }
+
+    function hideWatch(){
+      $('#watch-button-LKBG').css('display', 'none');
+      $('#stop-button-LKBG').css('display', 'inline-block');
+    }
+
+    function showWatch(){
+      $('#watch-button-LKBG').css('display', 'inline-block');
+      $('#stop-button-LKBG').css('display', 'none');
+    }
+
+    function showClear(){
+      $('#clear-button-LKBG').css('display', 'inline-block');
+    }
+
+    function hideClear(){
+      $('#clear-button-LKBG').css('display', 'none');
+    }
+
+    function saveKeyCombo(event, self){
+      var comboKeys = tempKey.join(' ');
+      if(comboKeys !== ''){
+        keyCombos.push(comboKeys);
+        displayKeyCombos();
+        clearSavedKey();
+        showClear();
+      }
+    }
+
+    function displayKeyCombos(){
+      keyComboDisplay = prepareKeyCombos();
+      $('#current-keys').html(keyComboDisplay);
+    }
+
+    function prepareKeyCombos(){
+      keyComboDisplay = [];
+      for(i = 0; i < keyCombos.length; i++){
+        keyComboDisplay.push('<li>' + keyCombos[i] + '</li>');
+      }
+      keyComboDisplay.join('');
+      return keyComboDisplay;
+    }
+
+    function clearSavedKey(){
+      savedKeys = [];
+    }
+
+
+    function clearKeyCombos(){
+      hideClear();
+      keyCombos = [];
+      displayKeyCombos();
+    }
+
+    jQuery('#box-container-LKBG').draggable();
